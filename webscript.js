3d73b6b8f2be8d20299b4e3cd3a3ddfb2418358b
Siddhant Sharma <siddhaantsharma.ss@gmail.com>
2017-12-16 22:06:15 +0530

    Updated Webscript Javascript.

---
+++ webscript.js
@@ -0,0 +1,6 @@
+ function my() {
+            var usertext = document.f1.t1.value.toLowerCase();
+            var utext = document.f1.w1.value;
+            var ustext = document.f1.e1.value;
+            window.open("https://image.thum.io/get/width/" + utext + "/crop/" + ustext + "/allowJPG" + "/noanimate" + "/http://" + usertext);
+        }
